8a07e38fd23f7ada9ceeff4a760d94e98c12ee50
Ibinayo <toyinibinayo20@gmail.com>
2022-10-19 06:50:52 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char lc;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+			_putchar(lc);
+		_putchar('\n');
+	}
+}
